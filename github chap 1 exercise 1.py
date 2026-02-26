900808226f73b81071b0d64949e8af6fb5c5d4e1
Mcjhayro <mcjhayrodtapales@gmail.com>
2026-02-26 21:04:53 +0800

    Delete github chap 1 exercise 1.py

--- github chap 1 exercise 1.py
+++
@@ -1,8 +0,0 @@
-print("Hello, user!")
-name = input("What is your name?\n").title()
-age = int(input("What is your age?\n"))
-
-print(f"It is good to meet you, {name}")
-print("The length of your name is:")
-print(len(name))
-print(f"You will be {age + 1} in a year.")
