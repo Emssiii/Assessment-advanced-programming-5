eeace4630d2bd78064587e2cb498c12326fbfcfa
Mcjhayro <mcjhayrodtapales@gmail.com>
2026-02-26 21:08:40 +0800

    Delete github chap 1 exercise 5.py

--- github chap 1 exercise 5.py
+++
@@ -1,6 +0,0 @@
-count = 0
-choice = input("Do you want to continue? (Y/N): ").upper()
-while choice == 'Y':
-    count += 1
-    choice = input("Do you want to continue? (Y/N): ").upper()
-print("Loop executed", count, "times.\n")
