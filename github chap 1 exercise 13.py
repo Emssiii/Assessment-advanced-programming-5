a406afc5dc22f24f20c644c22c851e2b1b425deb
Mcjhayro <mcjhayrodtapales@gmail.com>
2026-02-26 21:06:58 +0800

    Delete github chap 1 exercise 13.py

--- github chap 1 exercise 13.py
+++
@@ -1,9 +0,0 @@
-def product_of_list(numbers):
-    product = 1
-    for num in numbers:
-        product *= num
-    return product
-
-nums = [2, 3, 4, 5]
-print("List:", nums)
-print("Product of list items:", product_of_list(nums), "\n")
